652e7d911ecfff6f7e6e28527bfee70da400e1e3
naoya <naoyakumagai1999@gmail.com>
2021-10-10 17:16:44 -0700

    bridged camera tfs

--- elevation_mapping_demos/scripts/odom_to_tracking_cam_pose.py
+++ elevation_mapping_demos/scripts/odom_to_tracking_cam_pose.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
+#http://wiki.ros.org/tf2/Tutorials/Quaternions
+#https://github.com/IntelRealSense/realsense-ros/blob/bb5c713d57c41c30609862591d778f4c33abd695/realsense2_camera/urdf/mount_t265_d435.urdf.xacro
 
 import rospy
 import tf
 import tf2_ros
 import geometry_msgs.msg
 from nav_msgs.msg import Odometry
+from tf.transformations import quaternion_from_euler
 
 class ROSController:
 
     def __init__(self):
        # Obtain odom values to create transformation frame between base and world
        self.sub_tf_world = rospy.Subscriber('/cam_tracking/odom/sample', Odometry, self.Callback_tf_world)
+       #self.q = quaternion_from_euler(0.000, -0.018, 0.005)
        
     def Callback_tf_world(self, msg):
         # this function will broadcast the world frame relative to the base frame. This is required if we want to do
@@ -18,19 +22,22 @@ class ROSController:
         tf_world_broadcaster = tf2_ros.TransformBroadcaster()
         tf_world = geometry_msgs.msg.TransformStamped()
         tf_world.header.stamp = rospy.Time.now()
-        tf_world.header.frame_id = "map"
-        tf_world.child_frame_id = "cam_tracking_pose_frame" #"t265_odom_frame" #"odom"
-        tf_world.transform.translation.x = msg.pose.pose.position.x
-        tf_world.transform.translation.y = msg.pose.pose.position.y
-        tf_world.transform.translation.z = msg.pose.pose.position.z
-        tf_world.transform.rotation.x = msg.pose.pose.orientation.x
-        tf_world.transform.rotation.y = msg.pose.pose.orientation.y
-        tf_world.transform.rotation.z = msg.pose.pose.orientation.z
-        tf_world.transform.rotation.w = msg.pose.pose.orientation.w
+        tf_world.header.frame_id = "odom"
+        tf_world.child_frame_id = "/cam_tracking_odom_frame"
+        tf_world.transform.translation.x = 0	
+        tf_world.transform.translation.y = 0         
+        tf_world.transform.translation.z = 0
+        tf_world.transform.rotation.x = 0
+        tf_world.transform.rotation.y = 0
+        tf_world.transform.rotation.z = 0
+        tf_world.transform.rotation.w = 1
+   
+   
         tf_world_broadcaster.sendTransform(tf_world) 
         
 if __name__ == '__main__':
-  rospy.init_node('camera_tf')
+  rospy.init_node('odom_to_tracking_pose_tf_broadcaster')
+  
   roscontroller = ROSController()
   rospy.spin()
    

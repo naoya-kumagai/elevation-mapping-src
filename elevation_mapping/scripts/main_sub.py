66da8acb6044c12c5906db4f874875f7a17e8822
naoya <naoyakumagai1999@gmail.com>
2021-11-21 07:16:56 -0800

    Added masking function

--- elevation_mapping/scripts/main_sub.py
+++ elevation_mapping/scripts/main_sub.py
@@ -7,11 +7,13 @@ from elevation_map_sub import listener
 import rospy
 # from std_msgs.msg import String
 import numpy as np
+import numpy.ma as ma
 import cv2
 # import sys
 # import os
 # import time
 import matplotlib.pyplot as plt
+#from collections import Counter
 
 def height_map_plotter(fig, ax, height_map, resolution, cmap, first_loop, vmin, vmax):
     extent = [0, height_map.shape[1]*resolution, 0, height_map.shape[0]*resolution]
@@ -19,7 +21,6 @@ def height_map_plotter(fig, ax, height_map, resolution, cmap, first_loop, vmin, 
         ax.grid()
         pos = ax.imshow(height_map,cmap=cmap, vmin =vmin, vmax =vmax, extent=extent)
         cbar = fig.colorbar(pos, ax=ax, shrink=0.5)
-        #ax.set_ylabel('Y Label')
         ax.set_title('Elevation map')
         ax.set_xlabel('map width (m)')
         ax.set_ylabel('map length (m)')
@@ -35,8 +36,41 @@ def plot_height_dist(fig, ax, height_map, first_loop):
     ax.set_title('Elevation distribution')
     ax.set_ylabel('number of cells')
     ax.set_xlabel('elevation (m)')
-    ax.set_xlim(-0.44, -0.41)
-    ax.hist(np.reshape(height_map, (-1,1)), bins = 30)
+    #ax.set_xlim(-0.44, -0.41) #for plane only
+    ax.set_xlim(-0.44, -0.35)
+    ax.hist(np.reshape(height_map, (-1,1)), bins = 100)
+
+def get_plane_height_mean(height_map):
+    min = -0.44
+    max = -0.34
+    hist, bin_edges = np.histogram(np.reshape(height_map, (-1,1)),bins=np.linspace(min,max, 100), density=False)
+    plane_height_mean = bin_edges[np.argmax(hist)]
+    return plane_height_mean
+
+def detect_from_dist(fig, ax, height_map, plane_height_mean, resolution, cmap, first_loop, vmin, vmax):
+    '''
+    Creates masking of height map based on the distribution of the data.
+    This assumes that the plane is the dominant surface area, which is reasonable for the current climbing wall.
+    '''
+    variance = 0.005 # assumes variance of plane data is 5mm. TODO: improve this!
+
+    plane_height_upper_bound = plane_height_mean + variance
+
+    height_map_masked = np.where(height_map > plane_height_upper_bound, height_map, 0 )
+
+    extent = [0, height_map.shape[1]*resolution, 0, height_map.shape[0]*resolution]
+    if first_loop:
+        ax.grid()
+        pos = ax.imshow(height_map_masked,cmap=cmap, vmin =vmin, vmax =vmax, extent=extent)
+        cbar = fig.colorbar(pos, ax=ax, shrink=0.5)
+        ax.set_title('Masked elevation map')
+        ax.set_xlabel('map width (m)')
+        ax.set_ylabel('map length (m)')
+    
+    else:
+        ax.imshow(height_map_masked,cmap=cmap, vmin =vmin, vmax =vmax, extent=extent)
+    
+
 
 
 def online_plotting(save_map):
@@ -44,7 +78,7 @@ def online_plotting(save_map):
 
     my_listener = listener() #the height map would be a top-down view
 
-    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, constrained_layout=True)
+    fig, (ax1, ax2,ax3) = plt.subplots(nrows=1, ncols=3, figsize=(15,15), constrained_layout=True)
     # model, dataset, config = inference_init(ROOT_DIR=ROOT_DIR, HOLD_DIR='/home/yusuke/Mask_RCNN/hold/dataset2', 
     #     subset='val',weights_path='/home/yusuke/Mask_RCNN/logs/mask_rcnn_hold_0010.h5')
  
@@ -64,7 +98,6 @@ def online_plotting(save_map):
     true_b = 0.03
     
     i = 0
-
             
     while not rospy.is_shutdown():
         #rospy.sleep(0.1)
@@ -90,14 +123,16 @@ def online_plotting(save_map):
             vmax = vmin + 0.10
             vmax=0
 
-            vmin = None
-            vmax = None
+            # vmin = None
+            # vmax = None
 
             height_map_plotter(fig, ax1, my_listener.height_map, my_listener.resolution, cmap, first_loop, vmin, vmax)
 
-            #plot_height_dist(fig, ax2, my_listener.height_map, first_loop)
+            plot_height_dist(fig, ax2, my_listener.height_map, first_loop)
 
-       
+            plane_height_mean = get_plane_height_mean(my_listener.height_map)
+
+            detect_from_dist(fig, ax3,my_listener.height_map, plane_height_mean, my_listener.resolution, cmap, first_loop, vmin, vmax)
 
             if save_map:
                 # np.save("/home/yusuke/height_map.npy", my_listener.height_map)
